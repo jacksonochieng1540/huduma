18d85961ef1a3ebd683bc8585834620d2d44b251
jack ayaga <jacksonochieng@hmail.com>
2025-07-19 14:17:03 +0300

    update the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,16 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Creating superuser if it doesn't exist..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "admin"
+email = "admin@example.com"
+password = "adminpass"
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+EOF
+
 echo "Starting Gunicorn server..."
 exec gunicorn huduma.wsgi:application --bind 0.0.0.0:8000
