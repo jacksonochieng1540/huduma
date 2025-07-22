7cc6b32df71821bdb872b95f21cd1653a43036ef
Jack ayaga <jacksonochieng1540@gmail.com>
2025-07-22 22:07:39 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ echo "Creating superuser if it doesn't exist..."
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-username = "admin"
+username = "Vettle"
 email = "admin@example.com"
-password = "adminpass"
+password = "members"
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
 EOF
